cd0dd16edea3311e95b856d660ec0b18b7dfaef5
yuliagiarto <yuliagiarto@funpodium.net>
2020-05-17 20:25:22 +0800

    section 15 end. basic redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import classes from "./ContactData.css";
 import Button from "../../../components/UI/Button/Button";
 import axios from "../../../axios-orders";
@@ -129,7 +130,7 @@ class ContactData extends Component {
       formData[formElemIdenfier] = this.state.orderForm[formElemIdenfier].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
     };
@@ -143,7 +144,7 @@ class ContactData extends Component {
       .catch((error) => {
         this.setState({ loading: false });
       });
-    console.log(this.props.ingredients);
+    console.log(this.props.ings);
   };
 
   checkValidity(value, rules) {
@@ -233,4 +234,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
